a64a46f9fdcc464353798943a2bc33a3248ec471
harihar90 <harihar90@gmail.com>
2015-04-29 22:45:07 -0500

    multiple runs -code fixes

--- src/de/huberlin/wbi/dcs/examples/Parameters.java
+++ src/de/huberlin/wbi/dcs/examples/Parameters.java
@@ -319,12 +319,12 @@ public class Parameters {
 
 	}
 
-<<<<<<< HEAD
+
 	public enum Gaming {
 		OFF, BASIC, BASIC_WITH_MIGRATION, OPPORTUNISTIC
 	}
 
-	static Gaming game = Gaming.OPPORTUNISTIC;
+	
 
 	public enum Charging {
 		NULL, COUNTERACT_GAMING
@@ -333,9 +333,8 @@ public class Parameters {
 	public static Charging charging = Charging.NULL;
 	public static final int nUsers = 1;
 	public static final long RECHECK_LIMIT = 100;
-=======
+
 	static Gaming game=Gaming.OPPORTUNISTIC;
-	public enum Charging{NULL,COUNTERACT_GAMING}
-	public static Charging charging = Charging.NULL;
->>>>>>> 34486891a339d4a05cce88822a09a6142c953bf3
+	
+
 }
